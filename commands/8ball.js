bf4b3d4c6cb925e98d8acfcf7f144119b6e8a715
Butterstroke <konirosen@protonmail.ch>
2017-10-07 19:09:23 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,24 +1,18 @@
 const Discord = require('discord.js');
+
 exports.run = function(client, message, args) {
-if(message.author.bot) return;
-if (!message.guild || !message.member) return;
-var reason = message.content.split(' ')
-reason.shift()
-var reason = reason.join(' ');
-var reason = reason = reason.replace("?", "");
-if (!reason) {
-const embed = new Discord.RichEmbed()
-.setColor(0xFFB200)
-.setTimestamp()
-.addField(':8ball: 8ball', "I can't tell with nothingness. Please try again :wink:.")
-.setFooter(`8balled by ${message.author.username}`)
-return message.channel.sendEmbed(embed);
-}
-var response = [`**${reason}?** **No** **${message.author.username}.** Just no.`, `**${reason}?** **Yes**. A big **Y E S** **${message.author.username}!**`, `**${reason}?** Maybe. Who knows amirite?\n*besides you* **${message.author.username}** :wink:.`, `**${reason}?** Possibly but for now, no **${message.author.username}.**`, `**${reason}?** Well um it could be possible **${message.author.username}.**`];
-const embed = new Discord.RichEmbed()
-.setColor(0xFFB200)
-.setTimestamp()
-.addField(':8ball: 8ball', response[~~(Math.random() * response.length)])
-.setFooter(`8balled by ${message.author.username}`)
-return message.channel.sendEmbed(embed);
+  	if (!message.guild || !message.member) return;
+  	var reason = args.join(' ');
+	const embed = new Discord.RichEmbed()
+    	.setColor(0xFFB200)
+      	.setTimestamp()
+		.setFooter(`8balled by ${message.author.username}`);
+	
+  if (!reason) {
+      	embed.addField(':8ball: 8ball', "I can't tell with nothingness. Please try again :wink:.")
+   	return message.channel.send({embed});
+  }
+  	var response = [`**${reason}?** **No** **${message.author.username}.** Just no.`, `**${reason}?** **Yes**. A big **Y E S** **${message.author.username}!**`, `**${reason}?** Maybe. Who knows amirite?\n*besides you* **${message.author.username}** :wink:.`, `**${reason}?** Possibly but for now, no **${message.author.username}.**`, `**${reason}?** Well um it could be possible **${message.author.username}.**`];
+		embed.addField(':8ball: 8ball', response[~~(Math.random() * response.length)]);
+	return message.channel.send({embed});
 }
