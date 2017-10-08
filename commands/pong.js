0647f4a1d2935e6db69f4a042ee583c276e3848c
Butterstroke <konirosen@protonmail.ch>
2017-10-08 00:30:30 -0400

    Update pong.js

--- commands/pong.js
+++ commands/pong.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
+
 exports.run = function(client, message) {
-message.reply('pong? What... um. Ping?');
-var delayMillis = 10000;
-setTimeout(function() {
-  const embed = new Discord.RichEmbed()
-  .setColor(0xFFB200)
-  .setTimestamp()
-  .addField(':ping_pong:', 'I ponged back at the speed of ' + Math.round(message.client.ping) + ' ms!')
-  .setFooter(`Pinged by ${message.author.username}`)
-  message.channel.sendEmbed(embed);
-  message.channel.send('Welp :3')
+  message.reply('pong? What... um. Ping?');
+  var delayMillis = 10000;
+  setTimeout(function() {
+    const embed = new Discord.RichEmbed()
+      .setColor(0xFFB200)
+      .setTimestamp()
+      .addField(':ping_pong:', 'I ponged back at the speed of ' + Math.round(message.client.ping) + ' ms!')
+      .setFooter(`Pinged by ${message.author.username}`);
+    message.channel.send({embed});
+    message.channel.send('Welp :3');
   }, delayMillis);
 }
