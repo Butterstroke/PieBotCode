88626b467525190201c91c7ce52d508b44911194
Butterstroke <konirosen@protonmail.ch>
2017-10-08 00:32:21 -0400

    Update im.js

--- commands/im.js
+++ commands/im.js
@@ -1,8 +1,6 @@
 exports.run = function(client, message, args) {
-let reason = msg.content.split(' ').slice(1).join(' ');
-if (!reason) {
-return message.channel.sendMessage(`Hi ${message.author.username}, I'm Pie!`);
-} else {
-message.channel.sendMessage(`Hi ${reason}, I'm Pie!`)
-}
+  let reason = msg.content.split(' ').slice(1).join(' ');
+  if (!reason) { return message.channel.send(`Hi ${message.author.username}, I'm Pie!`); } 
+
+  message.channel.send(`Hi ${reason}, I'm Pie!`);
 }
