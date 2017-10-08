239fe18d48e5334965f13b74457335c15356f53a
Butterstroke <konirosen@protonmail.ch>
2017-10-08 13:24:50 -0400

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -1,66 +1,58 @@
 const Discord = require('discord.js');
+
 exports.run = function(client, message, args) {
-if (message.author.bot) return;
-var user = message.mentions.users.first();
-var member = message.guild.member(user);
-let reason = message.content.trim().split(' ').slice(2).join(' ');
-if (!user) {
+    var user = message.mentions.users.first();
+    var member = message.guild.member(user);
+    let reason = message.content.trim().split(' ').slice(2).join(' ');
+    
+    if (!user) { user = message.author; }
+    
+    const embed = new Discord.RichEmbed()
+        .setColor(0xFFB200)
+        .setTimestamp()
+        .addField(':cookie:', `<@${user.id}> check dms!`);
+    
     const embed1 = new Discord.RichEmbed()
         .setColor(0xFFB200)
-        .setTimestamp()
-        .addField(':cookie:', `<@${message.author.id}> check dms!`)
-        .setFooter(`Cookied by ${message.author.username}`);
-    message.channel.sendEmbed(embed1)
-        .then(x => DeleteQueue.add(x, 5000));
-    const embed2 = new Discord.RichEmbed()
-        .setColor(0xFFB200)
-        .setTimestamp()
-        .addField(':cookie:', `You have received a :cookie: from <@288221668290068480>!`)
-        .setFooter(`Sent from Pie!`);
-    return message.author.sendEmbed(embed2);
-} else if (!reason) {
-    const embed3 = new Discord.RichEmbed()
-        .setColor(0xFFB200)
-        .setTimestamp()
-        .addField(':cookie:', `<@${user.id}> check dms!`)
-        .setFooter(`Cookied by ${message.author.username}`);
-    message.channel.sendEmbed(embed3)
-        .then(x => DeleteQueue.add(x, 5000));
-    const embed4 = new Discord.RichEmbed()
-        .setColor(0xFFB200)
-        .setTimestamp()
-        .addField(':cookie:', `You have received a :cookie: from <@${message.author.id}>!`)
-        .setFooter(`Sent from ${message.author.username}`);
-    user.sendEmbed(embed4)
-        .then(() => {
+        .setTimestamp();
+    
+    if (!user) {
+        embed.setFooter(`Cookied by ${user.username}`);
+        message.channel.send({embed: embed}).then(x => DeleteQueue.add(x, 5000));
+        embed.addField(':cookie:', `You have received a :cookie: from <@288221668290068480>!`)
+            .setFooter(`Sent from Pie!`);
+        return message.author.send({embed: embed1});
+    } else if (!reason) {
+        embed.setFooter(`Cookied by ${message.author.username}`);
+        message.channel.send({embed}).then(x => DeleteQueue.add(x, 5000));
+        const embed4 = new Discord.RichEmbed()
+            .setColor(0xFFB200)
+            .setTimestamp()
+            .addField(':cookie:', `You have received a :cookie: from <@${message.author.id}>!`)
+            .setFooter(`Sent from ${message.author.username}`);
+        user.send({embed: embed4}).then(() => {
             const embed5 = new Discord.RichEmbed()
                 .setColor(0xFFB200)
                 .setTimestamp()
                 .addField(':cookie:', `You have sent a :cookie: to <@${user.id}>!`)
                 .setFooter(`Sent to ${user.username}`);
-            message.author.sendEmbed(embed5);
+            message.author.send({embed: embed5});
         });
 } else {
-    const embed3 = new Discord.RichEmbed()
-        .setColor(0xFFB200)
-        .setTimestamp()
-        .addField(':cookie:', `<@${user.id}> check dms!`)
-        .setFooter(`Cookied by ${message.author.username}`);
-    message.channel.sendEmbed(embed3)
-        .then(x => DeleteQueue.add(x, 5000));
+    embed.setFooter(`Cookied by ${message.author.username}`);
+    message.channel.send({embed}).then(x => DeleteQueue.add(x, 5000));
     const embed4 = new Discord.RichEmbed()
         .setColor(0xFFB200)
         .setTimestamp()
         .addField(':cookie:', `You have received a :cookie: from <@${message.author.id}> with a message saying ${reason}!`)
         .setFooter(`Sent from ${message.author.username}`);
-    user.sendEmbed(embed4)
-        .then(() => {
-            const embed5 = new Discord.RichEmbed()
-                .setColor(0xFFB200)
-                .setTimestamp()
-                .addField(':cookie:', `You have sent a :cookie: to <@${user.id}>!`)
-                .setFooter(`Sent to ${user.username}`);
-            message.author.sendEmbed(embed5);
-        });
-}
+    user.send({embed: embed4}).then(() => {
+    const embed5 = new Discord.RichEmbed()
+        .setColor(0xFFB200)
+        .setTimestamp()
+        .addField(':cookie:', `You have sent a :cookie: to <@${user.id}>!`)
+        .setFooter(`Sent to ${user.username}`);
+    message.author.send({embed: embed5});
+    });
+    }
 };
