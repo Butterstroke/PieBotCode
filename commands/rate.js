a4cda3a17da1fc84c9914ada35ab4a21032b06f5
Butterstroke <konirosen@protonmail.ch>
2017-10-08 00:40:23 -0400

    Update rate.js

--- commands/rate.js
+++ commands/rate.js
@@ -1,22 +1,19 @@
 const Discord = require("discord.js");
+
 exports.run = function(client, message, args) {
-var reason = message.content.split(' ')
-reason.shift()
-var reason = reason.join(' ');
-var reason = reason = reason.replace(".", "");
-if (!reason) {
-const embed = new Discord.RichEmbed()
-.setColor(0xFFB200)
-.setTimestamp()
-.addField(':mouse2: Rate', "I can't rate with nothingness. Please try again :wink:")
-.setFooter(`Rated by ${message.author.username}`)
-return message.channel.sendEmbed(embed);
-}
-var response = [`I would rate **${reason}** 1/10.`, `I would rate **${reason}** 2/10.`, `I would rate **${reason}** 3/10.`, `I would rate **${reason}** 4/10.`, `I would rate **${reason}** 5/10.`, `I would rate **${reason}** 6/10.`, `I would rate **${reason}** 7/10.`, `I would rate **${reason}** 8/10.`, `I would rate **${reason}** 9/10.`, `I would rate **${reason}** 10/10.`];
+  var reason = message.content.split(' ')
+  reason.shift()
+  var reason = reason.join(' ').replace(".", "");
   const embed = new Discord.RichEmbed()
-  .setColor(0xFFB200)
-  .setTimestamp()
-  .addField(':mouse2: Rate', response[~~(Math.random() * response.length)])
-  .setFooter(`Rated by ${message.author.username}.`)
-  message.channel.send({embed})
+    .setColor(0xFFB200)
+    .setTimestamp()
+    .setFooter(`Rated by ${message.author.username}`);
+  
+  if (!reason) {
+    embed.addField(':mouse2: Rate', "I can't rate with nothingness. Please try again :wink:");
+    return message.channel.sendEmbed(embed);
+  }
+  var response = ["1/10.", "2/10.", "3/10.", "4/10.", "5/10.", "6/10.", "7/10.", "8/10.", "9/10.", "10/10."];
+  embed.addField(':mouse2: Rate', `I would rate **${reason}** ${response[~~(Math.random() * response.length)]}`);
+  message.channel.send({embed});
 }
