899291c77ee11b9778e2c242ee526e2758cc873e
Butterstroke <konirosen@protonmail.ch>
2017-10-08 00:44:33 -0400

    Update rickroll.js

--- commands/rickroll.js
+++ commands/rickroll.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
+
 exports.run = function(client, message) {
-        const embed = new Discord.RichEmbed()
-        .setColor(0xFFB200)
+	const embed = new Discord.RichEmbed()
+    	.setColor(0xFFB200)
         .addField(`I will never give you up, `, `${message.author.username}!`)
-        .setImage("https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif")
-        return message.channel.sendEmbed(embed);
-      }
+        .setImage("https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif");
+     return message.channel.send({embed});
+}
