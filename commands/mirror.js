745ae89d9692e59b1945e0c031cab08db5cff8f9
Butterstroke <konirosen@protonmail.ch>
2017-10-09 17:52:01 -0400

    Update mirror.js

--- commands/mirror.js
+++ commands/mirror.js
@@ -1,17 +1,16 @@
 var Jimp = require("jimp");
-exports.run = function(client, message, args) {
-  let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
 
-     if (imageurl === undefined) {
-         message.channel.send("**Please attach an image, or give a url to an image**");
-         return;
+exports.run = function(client, message, args) {
+    let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
+
+    if (imageurl === undefined) {
+        return message.channel.send("**Please attach an image, or give a url to an image**");
      }
 
      imageurl = imageurl.url;
 
      Jimp.read(imageurl).then(function (image) {
-image.mirror(180, 0)
-             .getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+        image.mirror(180, 0).getBuffer(Jimp.MIME_PNG, (error, buffer) => {
              message.channel.send("", {
                  files: [{
                      attachment: buffer,
