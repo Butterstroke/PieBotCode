dc83888050df66ee4af9b73db31d2ff35272ec64
Butterstroke <konirosen@protonmail.ch>
2017-10-08 20:25:51 -0400

    Update radio.js

--- commands/radio.js
+++ commands/radio.js
@@ -1,258 +1,107 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const ffmpeg = require('ffmpeg');
+
 exports.run = function(client, message, args) {
-  if (message.content === 'p.radio mix') {
+	if (args === "mix") { radioName = "Mix 102.3"; }
+    if (args === "nova919") { radioName = "Nova 91.9"; }
+    if (args === "fall1") { radioName = "Fallout Main Station"; }
+    if (args === "fall2") { radioName = "Fallout Classical Radio"; }
+    if (args === "fall3") { radioName = "Diamond City Radio"; }
+    if (args === "fall4") { radioName = "MWTCF"; }
+    if (args === "fall5") { radioName = "Galaxy News Radio"; }
+    if (args === "fall6") { radioName = "NV Radio (New Vegas)"; }
+    if (args === "kissfresh") { radioName = "Kiss Fresh"; }
+    if (args === "clyde1") { radioName = "Clyde1"; }
+    if (args === "coolfm") { radioName = "Cool FM"; }
+    if (args === "iloveradio1") { radioName = "ILoveRadio 1"; }
+    if (args === "iloveradio2") { radioName = "ILoveRadio 2"; }
+    if (args === "pophits") { radioName = "Pop Hits"; }
+    if (args === "") { radioName = ""; }
+    if (args === "") { radioName = ""; }
+	
+	if (!message.member.voiceChannel) {
+		const embed = new Discord.RichEmbed()
+      		.setColor(0xFFB200)
+      		.addField(`:radio: | Before streaming ${radioName}...`, "Join a voice channel!")
+      	return message.channel.send({embed})
+	}
+	
+	const embed = new Discord.RichEmbed()
+    	.setColor(0xFFB200)
+        .addField(':radio: | Now streaming...', `**${radioName}!**`);
+	
+	if (message.content === 'p.radio mix') {
+      	message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://icecast.arn.com.au/1023.mp3', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+  	} else if (message.content === 'p.radio nova919') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://117.53.162.168/nova919', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio fall1') {
     if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://icecast.arn.com.au/1023.mp3', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Mix 102.3!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Mix 102.3...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  if (message.content === 'p.radio nova919') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://117.53.162.168/nova919', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Nova 91.9**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Nova 91.9...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	    if (message.content === 'p.radio fall1') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm1.ogg', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Fallout Main Station!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Fallout Main Station...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  if (message.content === 'p.radio fall2') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm7.ogg', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Fallout Classical Radio!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Fallout Classical Radio...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  if (message.content === 'p.radio fall3') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm6.ogg', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Diamond City Radio!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Diamond City Radio...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  if (message.content === 'p.radio fall4') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm8.ogg', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**MWTCF!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming MWTCF...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  if (message.content === 'p.radio fall5') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm2.ogg', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Galaxy News Radio!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Galaxy News Radio...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  if (message.content === 'p.radio fall6') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm3.ogg', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**NV Radio (New Vegas)!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming NV Radio (New Vegas)...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  	  if (message.content === 'p.radio kissfresh') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://icy-e-bl-06-cr.sharp-stream.com:8000/kissfresh.mp3', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Kiss Fresh!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Kiss Fresh...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  	  	  if (message.content === 'p.radio clyde1') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://icy-e-bl-07-boh.sharp-stream.com/clyde1.mp3', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Clyde1!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Clyde1...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  	  	  	  if (message.content === 'p.radio coolfm') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://icy-e-bl-09-boh.sharp-stream.com/coolfm.mp3', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Cool FM!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Cool FM...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  	  	  	  	  if (message.content === 'p.radio iloveradio1') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://stream06.iloveradio.de/iloveradio5.mp3', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**ILoveRadio 1!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming ILoveRadio 1...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  	  	  	  	  	  if (message.content === 'p.radio iloveradio2') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://stream06.iloveradio.de/iloveradio2.mp3', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**ILoveRadio 2!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming ILoveRadio 2...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
-  } else {
-	  	  	  	  	  	  	  if (message.content === 'p.radio pophits') {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          const dispatcher = connection.playStream('http://rfcmedia.streamguys1.com/newpophits.aac', {passes: 4});
-          const embed = new Discord.RichEmbed()
-          .setColor(0xFFB200)
-          .addField(':radio: | Now streaming...', "**Pop Hits!**")
-          message.channel.sendEmbed(embed)
-        })
-        .catch(console.log);
-    } else {
-      const embed = new Discord.RichEmbed()
-      .setColor(0xFFB200)
-      .addField(':radio: | Before streaming Pop Hits...', "Join a voice channel!")
-      message.channel.sendEmbed(embed)
-    }
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm1.ogg', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio fall2') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm7.ogg', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio fall3') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm6.ogg', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio fall4') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm8.ogg', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio fall5') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm2.ogg', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio fall6') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://46.101.243.245:8000/falloutfm3.ogg', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio kissfresh') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://icy-e-bl-06-cr.sharp-stream.com:8000/kissfresh.mp3', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio clyde1') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://icy-e-bl-07-boh.sharp-stream.com/clyde1.mp3', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio coolfm') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://icy-e-bl-09-boh.sharp-stream.com/coolfm.mp3', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio iloveradio1') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://stream06.iloveradio.de/iloveradio5.mp3', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio iloveradio2') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://stream06.iloveradio.de/iloveradio2.mp3', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
+    } else if (message.content === 'p.radio pophits') {
+        message.member.voiceChannel.join().then(connection => { // Connection is an instance of VoiceConnection
+            const dispatcher = connection.playStream('http://rfcmedia.streamguys1.com/newpophits.aac', {passes: 4});
+            message.channel.send({embed});
+        }).catch(console.log);
   } else {
 	  if (message.content === 'p.radio kissfm') {
     if (message.member.voiceChannel) {
@@ -374,15 +223,3 @@ message.channel.sendEmbed(embed)
   }
   }
   }
-  }
-  }
-}
-}
-  }
-  }
-  }
-}
-  }
-  }
-  }
-}
