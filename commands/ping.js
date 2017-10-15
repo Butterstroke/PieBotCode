94b1afe758e9811d82bdf45cd1541790aba83ba8
Butterstroke <konirosen@protonmail.ch>
2017-10-15 16:27:51 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
+
 exports.run = function(client, message) {
-   const embed = new Discord.RichEmbed()
-   .setColor(0xFFB200)
-   .setTimestamp()
-   .addField(':ping_pong:', 'I ponged back at the speed of ' + Math.round(message.client.ping) + ' ms!')
-   .setFooter(`Pinged by ${message.author.username}`)
-   message.channel.sendEmbed(embed);
+	const embed = new Discord.RichEmbed()
+		.setColor(0xFFB200)
+		.setTimestamp()
+		.addField(':ping_pong:', 'I ponged back at the speed of ' + Math.round(message.client.ping) + ' ms!')
+		.setFooter(`Pinged by ${message.author.username}`)
+	message.channel.send({embed});
  };
