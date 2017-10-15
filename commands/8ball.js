49366ef458f4a7592229c88d379f1794cce9a6a8
Butterstroke <konirosen@protonmail.ch>
2017-10-15 17:02:32 -0400

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,18 +1,19 @@
 const Discord = require('discord.js');
 
 exports.run = function(client, message, args) {
-  	if (!message.guild || !message.member) return;
+  	if (!message.guild) { return; } 
   	var reason = args.join(' ');
 	const embed = new Discord.RichEmbed()
     	.setColor(0xFFB200)
       	.setTimestamp()
-	.setFooter(`8balled by ${message.author.username}`);
+		.setFooter(`8balled by ${message.author.username}`);
 	
-  if (!reason) {
+	if (!reason) {
       	embed.addField(':8ball: 8ball', "I can't tell with nothingness. Please try again :wink:.")
    	return message.channel.send({embed});
   }
   	var response = [`**${reason}?** **No** **${message.author.username}.** Just no.`, `**${reason}?** **Yes**. A big **Y E S** **${message.author.username}!**`, `**${reason}?** Maybe. Who knows amirite?\n*besides you* **${message.author.username}** :wink:.`, `**${reason}?** Possibly but for now, no **${message.author.username}.**`, `**${reason}?** Well um it could be possible **${message.author.username}.**`];
 	embed.addField(':8ball: 8ball', response[~~(Math.random() * response.length)]);
+	
 	return message.channel.send({embed});
 }
