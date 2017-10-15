4e538511bd95fe3a65f9602bbbd9e6f1709290a2
Butterstroke <konirosen@protonmail.ch>
2017-10-15 17:40:29 -0400

    Update contact.js

--- commands/contact.js
+++ commands/contact.js
@@ -2,8 +2,10 @@ const Discord = require('discord.js');
 
 exports.run = function(client, message, args) {
 	var reason = args.join(' ');
-	let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
 	const embed = new Discord.RichEmbed()
+		.setColor(0xFFB200);
+		
+	const embed1 = new Discord.RichEmbed()
 		.setColor(0xFFB200);
 	
 	if (!reason) { 
@@ -11,15 +13,9 @@ exports.run = function(client, message, args) {
 		return message.channel.send({embed});
 	}
 	
-	if (imageurl === undefined) {
-		embed.addField('Sent!', "Your message has been sent to Pie#9196!")
-		message.channel.send({embed});
-		embed.addField(`From: ${message.author.username}`, `${reason}`)
-		client.users.get('265071448182358018').send({embed});
-	} else {
-		embed.addField('Sent!', "Your message has been sent to Pie#9196!")
-		message.channel.send({embed});
-		embed.addField(`From: ${message.author.username}`, `${reason}` + `${imageurl}`)
-		client.users.get('265071448182358018').send({embed});
-	}
+	embed.addField('Sent!', "Your message has been sent to Pie#9196!");
+	embed1.addField(`From: ${message.author.username}`, `${reason}`);
+	
+	message.channel.send({embed});
+	client.users.get('265071448182358018').send({embed: embed1});
 }
