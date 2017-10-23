c0071198641d56f36e529e39b7d9fac16923b701
Butterstroke <konirosen@protonmail.ch>
2017-10-22 20:52:10 -0400

    Update jpeg.js

--- commands/jpeg.js
+++ commands/jpeg.js
@@ -1,23 +1,15 @@
 var Jimp = require("jimp");
+
 exports.run = function(client, message, args) {
-  let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
+	let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
 
-     if (imageurl === undefined) {
-         message.channel.send("**Please attach an image, or give a url to an image**");
-         return;
-     }
+    if (imageurl === undefined) { return message.channel.send("**Please attach an image, or give a url to an image**"); }
 
-     imageurl = imageurl.url;
+    imageurl = imageurl.url;
 
-     Jimp.read(imageurl).then(function (image) {
-        image.quality(10)
-             .getBuffer(Jimp.MIME_JPEG, (error, buffer) => {
-             message.channel.send("", {
-                 files: [{
-                     attachment: buffer,
-                     name: "pixel.jpeg"
-                 }]
-             });
-         });
-     });
-  }
+    Jimp.read(imageurl).then(function (image) {
+		image.quality(10).getBuffer(Jimp.MIME_JPEG, (error, buffer) => {
+            message.channel.send("", { files: [{ attachment: buffer, name: "pixel.jpeg" }] });
+        });
+    });
+}
