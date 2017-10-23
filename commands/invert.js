e948e0c05e8eb9d136e8074515a168c6f6546325
Butterstroke <konirosen@protonmail.ch>
2017-10-22 20:37:32 -0400

    Update invert.js

--- commands/invert.js
+++ commands/invert.js
@@ -1,23 +1,15 @@
-var Jimp = require("jimp");
+const Jimp = require("jimp");
+
 exports.run = function(client, message, args) {
-  let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
+	let imageurl = args[0] === undefined ? message.attachments.first() : { url: args[0] };
 
-     if (imageurl === undefined) {
-         message.channel.send("**Please attach an image, or give a url to an image**");
-         return;
-     }
+    if (imageurl === undefined) { return message.channel.send("**Please attach an image, or give a url to an image**"); }
 
-     imageurl = imageurl.url;
+    imageurl = imageurl.url;
 
-     Jimp.read(imageurl).then(function (image) {
-       image.invert()
-             .getBuffer(Jimp.MIME_PNG, (error, buffer) => {
-             message.channel.send("", {
-                 files: [{
-                     attachment: buffer,
-                     name: "invert.png"
-                 }]
-             });
-         });
-     });
-  }
+    Jimp.read(imageurl).then(function (image) {
+		image.invert().getBuffer(Jimp.MIME_PNG, (error, buffer) => {
+			message.channel.send("", { files: [{ attachment: buffer, name: "invert.png" }] }); 
+		});
+    });
+}
