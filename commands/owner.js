eb2b77c15d77af03212375499a4aa5b7194591fc
Butterstroke <konirosen@protonmail.ch>
2017-10-27 20:36:05 -0400

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,14 +1,15 @@
+const Discord = require("discord.js");
+
 exports.run = function(client, message) {
-  if (message.author.id !== '265071448182358018') {
-    const embed = new Discord.RichEmbed()
-      .setColor(0xFF0000)
-      .addField('Owner', 'You are not Pie#9196 ;-;');
-    return;
-  } else {
-    const embed = new Discord.RichEmbed()
-      .setColor(0x00FF0F)
-      .addField('Owner', `Hello Pie#9196!`);
-  }
+	const embed = new Discord.RichEmbed();
+	
+	if (message.author.id !== '265071448182358018') {
+		embed.setColor(0xFF0000)
+			.addField('Owner', 'You are not Pie#9196 ;-;');
+	} else {
+		embed.setColor(0x00FF0F)
+			.addField('Owner', `Hello Pie#9196!`);
+	}
   
-  message.channel.send({embed});
+	message.channel.send({embed});
 }
